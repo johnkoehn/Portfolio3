fc7d379c759c09d64ec5ec87068c13dbc44d451e
John Koehn <kingkoehn@gmail.com>
2016-04-30 12:54:13 -0500

    Account register now works. Login also dispalys error messages now.

--- app.js
+++ app.js
@@ -53,6 +53,56 @@ app.get('/game', function(req, res)
     }
 });
 
+app.get('/register', function(req, res)
+{
+    res.render('register.html');
+});
+
+app.get('/login', function(req, res)
+{
+    res.render('login.html');
+});
+
+app.post('/register', function(req, res)
+{
+    sess = req.session;
+
+    //check is the passwords are the same
+    if(req.body.password == req.body.password2)
+    {
+        //go through the file, and check usernames
+        var fs = require('fs');
+        var array = fs.readFileSync('users.txt').toString().split(",");
+        var unique_username = true;
+        for(i in array)
+        {
+            if((i % 3) == 0 && req.body.username == array[i])
+            {
+                res.end('Username already exists!!');
+                unique_username = false;
+                break;
+            }
+        }
+
+        if(unique_username)
+        {
+            //write the data to file
+            var string;
+            for(i in array)
+            {
+                string +=array[i] + ",";
+            }
+            string += req.body.username + "," + req.body.password + "," + "10000";
+            fs.writeFileSync("users.txt", string, 'utf8');
+            res.end('Account registered! Login or register another account');
+        }
+    }
+    else
+    {
+        res.end('Passwords don\'t match!!!');
+    }
+});
+
 app.post('/login', function(req, res)
 {
     sess = req.session;
